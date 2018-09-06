5bcf0de45def751ba45957d4a5ea96e993560c5b
g0dpa <highlight@korea.ac.kr>
2018-09-06 13:15:27 +0900

    Add sum modules

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "sum.h"
 
 int main(){
 	
---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include "sum.h"
+
+int sum(int n){
+	int res = 0;
+	for (int i=1;i<=n;i++){
+		res = res+i;
+	}
+	return res;
+
+}
+
